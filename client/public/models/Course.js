f90b1d39232a7b122411cd457ee63ae8cf2d4d04
Vishal <vishalvijay135@gmail.com>
2026-08-10 00:47:49 +0530

    feat: add mongoose database schemas

---
+++ client/public/models/Course.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    instructor: {
+      type: String,
+      required: true,
+    },
+
+    duration: {
+      type: Number,
+      required: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Course", courseSchema);
